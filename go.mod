5680837a255130e471da2e033e4ec4f083b62cb4
Tomasz Machalski <atm001@motorolasolutions.com>
2018-10-10 16:51:15 +0200

    Added integration with Consul

--- go.mod
+++ go.mod
@@ -1,6 +1,32 @@
 module github.com/toomyem/hello-world
 
 require (
+	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
+	github.com/hashicorp/consul v1.2.3
+	github.com/hashicorp/go-cleanhttp v0.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v0.0.0-20150518234257-fa3f63826f7c // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/hashicorp/go-rootcerts v0.0.0-20160503143440-6bb64b370b90 // indirect
+	github.com/hashicorp/go-sockaddr v0.0.0-20180320115054-6d291a969b86 // indirect
+	github.com/hashicorp/memberlist v0.1.0 // indirect
+	github.com/hashicorp/serf v0.8.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/miekg/dns v1.0.13 // indirect
+	github.com/mitchellh/go-homedir v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/pascaldekloe/goe v0.0.0-20180627143212-57f6aae5913c // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20181009213950-7c1a557ab941 // indirect
+	golang.org/x/net v0.0.0-20181005035420-146acd28ed58 // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	golang.org/x/sys v0.0.0-20181005133103-4497e2df6f9e // indirect
+	google.golang.org/appengine v1.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/vmihailenco/msgpack.v2 v2.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net"
+	"os"
 	"net/http"
 	"strings"
+	"github.com/hashicorp/consul/api"
 )
 
 func write(w http.ResponseWriter, fmt_ string, args ...interface{}) {
@@ -34,17 +36,49 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Health check performed (%s)\n", r.URL.Path)
+	fmt.Printf("Health check performed from %s (%s)\n", r.RemoteAddr, r.URL.Path)
 	w.WriteHeader(http.StatusOK)
 }
 
+func registerWithConsul() {
+    addr := os.Getenv("CONSUL_SERVICE_SERVICE_HOST") + ":" + os.Getenv("CONSUL_SERVICE_SERVICE_PORT")
+	fmt.Println("Registering with Consul...")
+	client, err := api.NewClient(&api.Config{
+		Address: addr,
+	})
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	agent := client.Agent()
+    addr = os.Getenv("HELLO_WORLD_SERVICE_SERVICE_HOST") + ":" + os.Getenv("HELLO_WORLD_SERVICE_SERVICE_PORT")
+	err = agent.ServiceRegister(&api.AgentServiceRegistration{
+		Name: "hello-world",
+		Port: 9000,
+		Address: "localhost",
+		Tags: []string{"http", "urlprefix=/hello strip=/hello"},
+		Check: &api.AgentServiceCheck{
+			Name: "hello-world-check",
+			HTTP: "http://" + addr + "/health",
+			Timeout: "2s",
+			Interval: "30s",
+		},
+	})
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	fmt.Printf("Registered!\n")
+}
+
 func main() {
-	fmt.Println("Starting app on port 9000")
+	go registerWithConsul()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/health", healthHandler)
 	r.HandleFunc("/", mainHandler)
 
+	fmt.Println("Starting app on port 9000")
 	err := http.ListenAndServe("0.0.0.0:9000", r)
 	if err != nil {
 		fmt.Printf("Cannot start app: %s\n", err)
